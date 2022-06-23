67f8db53641e1b9cbe9f749967a42a10a98d8288
SirJames115 <81270423+SirJames115@users.noreply.github.com>
2022-06-22 19:01:44 -0700

    Created html, css &  javascript files
    Printing my details to the screen

---
+++ script.js
@@ -0,0 +1,7 @@
+let firstName = 'James';
+let lastName = 'Othniel';
+let country = 'Nigeria';
+let height = '5.9ft';
+
+document.write(`<h1>JavaScript Task</h1> <h2> <small>Name:  ${firstName} ${lastName}  <br> height:  ${height} <br> Country: ${country} </small> </h2>`);
+console.log(`JavaScript Task \nName: ${firstName} ${lastName}\nheight:  ${height}\nCountry: ${country}`);
